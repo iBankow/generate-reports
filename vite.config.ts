5734553a135d68004a4a9e8bbc40b5a0fb2aead3
Tiago Bankow <tiago.bankow@gmail.com>
2026-01-05 09:06:50 -0400

    feat: Add gh-pages deployment script and update dependencies in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,4 +22,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  base: "/",
 })
